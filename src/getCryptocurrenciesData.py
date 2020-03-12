9507d535ac7ef30b405d7c496b19213d6574298a
codelardo <abelardo.dzib@beyondexperience.com>
2020-03-12 13:51:45 -0500

    added try catch on table read

--- src/getCryptocurrenciesData.py
+++ src/getCryptocurrenciesData.py
@@ -9,38 +9,42 @@ def get_coins_links(coins):
 	coin_links = []
 	for coin in coins:
 		link = settings.BASE_URL + coin + settings.URL_QUERY_STRING
-		coin_link = (coin, link)
+		coin_link = (coin.replace("-", "_"), link)
 		coin_links.append(coin_link)
 
 	return coin_links
 
 def get_history_table(coin, con):
-	coin_table = coin[0].replace("-", "_")
-	print('Get ' + coin_table + ' history')
+	print('Get ' + coin[0] + ' history')
 	db.create_table(coin[0], con)
 
 	site = requests.get(coin[1])
 	historical = BeautifulSoup(site.content, 'html.parser')
-	table = historical.find_all('table')[2]
+	try:
+		table = historical.find_all('table')[2]
+	except IndexError:
+		print("Error getting " + coin[0] + " data: DDoS protection by Cloudflare")
+		table = None
 
 	data = []
-	rows = table.find_all('tr')
-	for row in rows:
-	    items = []
-	    counted_rows = row.find_all('td')
-	    for element in counted_rows:	    	
-	    	items.append(element.text.strip())
-	    
-	    if items:	        
-	        date = items[0]
-	        date = time.mktime(datetime.datetime.strptime(date, '%b %d, %Y').timetuple())
-	        price = items[3].replace("$", "")
-	        price = price.replace(",", "")
-	        price = float(price)
-	        ratio = (date, price)
-	        data.append(ratio)
+	if table:
+		rows = table.find_all('tr')
+		for row in rows:
+		    items = []
+		    counted_rows = row.find_all('td')
+		    for element in counted_rows:	    	
+		    	items.append(element.text.strip())
+		    
+		    if items:	        
+		        date = items[0]
+		        date = time.mktime(datetime.datetime.strptime(date, '%b %d, %Y').timetuple())
+		        price = items[3].replace("$", "")
+		        price = price.replace(",", "")
+		        price = float(price)
+		        ratio = (date, price)
+		        data.append(ratio)
 	
-	data.reverse()
+		data.reverse()
 	return data
 
 def get_history(coins, con):
@@ -58,6 +62,6 @@ def main():
 	db.data_record(data_coin, connection)
 	connection.commit()
 	connection.close()
-		
+
 if __name__ == '__main__':
     main()
